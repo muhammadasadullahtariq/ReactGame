2dc9ad962c8ca3d8435e1a029d415b0bca304881
Asad ullah Tariq <mac@macs-MacBook-Pro.local>
2021-12-05 20:32:50 +0500

    Add the Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import Home from "./Pages/home";
 import ItemDetail from "./Pages/item_detail";
 import Collection from "./Pages/Collection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<ItemDetail />} />
+          <Route path="/collections" element={<Collection />} />
+        </Routes>
+      </BrowserRouter>
+    )
   );
 }
 
